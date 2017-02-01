d51a54ef9c0fa139942227e9c45aca928cfc4b7c
Lauren Arfaras <larfaras96@gmail.com>
2017-01-31 19:36:48 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var server = express();
+var port = process.env.PORT || 8080;
+
+// to serve css, images, and js
+server.use(express.static(__dirname + '/public'));
+
+server.get('/', function(request, response){
+  response.sendFile('index.html', {root: __dirname + '/public/html'});
+});
+
+server.listen(port, function(){
+  console.log("now listening on port", port);
+});
